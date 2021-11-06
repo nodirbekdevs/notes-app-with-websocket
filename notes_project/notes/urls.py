86fb726503c08e9e82ef9a1753f913c3a9891960
Nodir <93400178+nodirbekdevs@users.noreply.github.com>
2021-11-06 17:08:32 +0500

    Update urls.py

--- notes_project/notes/urls.py
+++ notes_project/notes/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.get_notes, name='get_notes'),
+    path('count/', views.get_note_count, name='get_note_count'),
     path('<int:pk>/', views.get_note, name='get_note'),
+    path('make/', views.make_note, name='make_note'),
+    path('edit/<str:pk>/', views.update_note, name='update_note'),
+    path('delete/<str:pk>/', views.delete_note, name='update_note'),
 ]
