d453e150a6cb283669df4c3c7cfbbc05167774b1
Nodir <93400178+nodirbekdevs@users.noreply.github.com>
2021-11-06 17:06:32 +0500

    Update views.py

--- notes_project/notes/views.py
+++ notes_project/notes/views.py
@@ -1,6 +1,5 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
 from .models import Note
 from .serializers import NoteSerializer
 
@@ -13,7 +12,43 @@ def get_notes(request):
 
 
 @api_view(['GET'])
+def get_note_counts(request):
+    notes = Note.objects.all().count()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
 def get_note(request, pk):
     note = Note.objects.get(id=pk)
     serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def make_note(request):
+    data = request.data
+    note = Note.objects.create(
+        title=data['title'],
+        content=data['content']
+    )
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def update_note(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    note.title=data['title']
+    note.content=data['content']
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def update_note(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response({message: 'Note was deleted'})
     return Response(serializer.data)
